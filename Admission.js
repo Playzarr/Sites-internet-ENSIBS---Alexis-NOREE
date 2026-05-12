706c61ec5bc098df90ed0d32e33fa1114e3f9ccf
Playzarr <alexis1.noree@gmail.com>
2026-05-12 16:32:18 +0200

    Add files via upload

---
+++ Admission.js
@@ -0,0 +1,3 @@
+function AffichageTexte() {
+    document.getElementById("Actualites").innerHTML = "<h2>L'ENSIBS DEVIENT LA 5ÈME ÉCOLE ASSOCIÉ AU RÉSEAU POLYTECH</h2><button id='button-Actualites' onclick='AffichageTexte()'>Découvrir nos actualités</button>";
+}
