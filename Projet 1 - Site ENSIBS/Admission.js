5efd0a5688e940708bfc50091217bdeaf5c45312
Playzarr <alexis1.noree@gmail.com>
2026-05-12 16:30:42 +0200

    Delete Projet 1 - Site ENSIBS directory

--- Projet 1 - Site ENSIBS/Admission.js
+++
@@ -1,3 +0,0 @@
-function AffichageTexte() {
-    document.getElementById("Actualites").innerHTML = "<h2>L'ENSIBS DEVIENT LA 5ÈME ÉCOLE ASSOCIÉ AU RÉSEAU POLYTECH</h2><button id='button-Actualites' onclick='AffichageTexte()'>Découvrir nos actualités</button>";
-}
